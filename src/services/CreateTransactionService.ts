ea8b6252bceba0f27bfc4649ee38581c45ec3d7f
Wellington <wellingtonslpbi@hotmail.com>
2020-06-24 20:47:20 -0300

    challenge finish 💻

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,11 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +13,26 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    const { total } = this.transactionsRepository.getBalance();
+
+    if (!['outcome', 'income'].includes(type)) {
+      throw new Error('The type entered is invalid');
+    }
+
+    if (type === 'outcome' && value > total) {
+      throw new Error(
+        'The amount reported is greater than that available in the account',
+      );
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
